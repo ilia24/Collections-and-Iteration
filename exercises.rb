17f7dcd08453a8f42bd82e857f4800cbb125388b
ilia24 <iliademertchian24@gmail.com>
2017-03-31 14:33:50 -0400

    Fill out hashes

--- exercises.rb
+++ exercises.rb
@@ -4,7 +4,7 @@ coin_flips = ['heads', 'tails', 'heads', 'tails', 'tails']
 artists = ["SRV", 'mark knopfler', 'john mayer']
 fav_colors_symbols = [:lime, :red, :purple]
 
-words = {}
-fav_movies = {}
-three_cities = {}
-friend_ages = {}
+words = {"the" => "denoting one or more people or things already mentioned or assumed to be common knowledge.", 'blue' => "of a color intermediate between green and violet, as of the sky or sea on a sunny day.", 'blase' => "unimpressed or indifferent to something because one has experienced or seen it so often before."}
+fav_movies = {'pulp ficton' => 1994, 'memento' => 1995, 'scarface' => 1983}
+three_cities = {'toronto' => 2600000, 'detroit' => 680000, 'dubai' => 2700000}
+friend_ages = {'alex' => 21, 'millie' => 23, 'johnnie' => 21}
